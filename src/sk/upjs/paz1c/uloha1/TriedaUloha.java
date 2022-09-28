43004e56de513d70d4a897e0251623a4294074d9
42190 <42190@158.197.239.45>
2022-09-28 17:39:09 +0200

    ahoj Github, vypis

--- src/sk/upjs/paz1c/uloha1/TriedaUloha.java
+++ src/sk/upjs/paz1c/uloha1/TriedaUloha.java
@@ -3,7 +3,9 @@ package sk.upjs.paz1c.uloha1;
 public class TriedaUloha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Ahoj github");
+		System.out.println("Helou githab");
 
 	}
 
